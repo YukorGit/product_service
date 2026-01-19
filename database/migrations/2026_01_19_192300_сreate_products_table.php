4a8f7c10c2813b61eb1259a07dee87b2f8701b49
YukorGit <yuriyseraz@gmail.com>
2026-01-19 23:18:18 +0300

    Persistence

---
+++ database/migrations/2026_01_19_192300_сreate_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('name')->index();
+            $table->decimal('price', 10, 2)->unsigned();
+            $table->boolean('in_stock')->default(true)->index();
+            $table->decimal('rating', 2, 1)->unsigned()->default(0.0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Infrastructure\Persistence\Eloquent\Models\ProductModel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,26 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        /*User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
+
+        $categories = ['Electronics', 'Books', 'Clothing', 'Toys', 'Flowers'];
+        $catIds = [];
+
+        foreach ($categories as $cat) {
+            $catIds[] = DB::table('categories')->insertGetId([
+                'name' => $cat,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        foreach ($catIds as $catId) {
+            ProductModel::factory()
+                ->count(60) // По 60 на категорию, 300 всего
+                ->create(['category_id' => $catId]);
+        }
     }
 }
