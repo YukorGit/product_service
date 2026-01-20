a1655b455ea541c27232d656ed4a597763e2032d
YukorGit <yuriyseraz@gmail.com>
2026-01-20 15:19:05 +0300

    api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Presenters\Api\V1\Controllers\ProductSearchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+Route::get('/products', [ProductSearchController::class, 'index']);
